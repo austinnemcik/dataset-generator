24d3619ebc48e14f1f2a9155294e9dac77e0e551
mosesfromdiscord <austinnemcik@gmail.com>
2026-02-21 03:50:10 -0500

    Initial setup of the API, database, and boilerplate code.

---
+++ database.py
@@ -0,0 +1,25 @@
+from sqlmodel import SQLModel, Field, Dataset, create_engine, Session, Relationship
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+_DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(_DATABASE_URL) # what we use to actually query postgres
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+class TrainingExample(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    instruction: str
+    response: str
+    dataset_id: int = Field(foreign_key="dataset.id")
+    dataset: Dataset = Relationship(back_populates="examples")
+
+class Dataset(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True) # this will automatically set an id if None
+    name: str
+    description: str | None = None
+    examples: list["TrainingExample"] = Relationship(back_populates="dataset")
+
---
+++ generics.py
@@ -0,0 +1,24 @@
+from fastapi.responses import JSONResponse
+
+def valid_http(statusCode):
+    if (statusCode != int):
+        return False
+    return (statusCode > 100 and statusCode <= 599)
+
+def response_builder(
+        success: bool, 
+        message: str, 
+        count: int | None = None, 
+        errors: int | None = None, 
+        statusCode: int = 200):
+    status = statusCode
+    if not (valid_http(statusCode)):
+        status = 500
+
+    return JSONResponse({
+        "success": success,
+        "message": message,
+        "amount": count,
+        "errors": errors
+    }, status_code=status)
+
